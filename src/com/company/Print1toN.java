102683700bde161d140bf3258be1da37c85bdd73
Shivam Singh <shivamfaceuit@gmail.com>
2021-10-14 10:42:40 +0530

    print a number from 1 to n

---
+++ src/com/company/Print1toN.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Print1toN {
+    public static void main(String[] args) {
+        print1toN(5);
+    }
+
+    private static void print1toN(int n) {
+        if (n==0){
+            return ;
+        }
+
+        print1toN(n-1);
+        System.out.println(n);
+
+    }
+}
