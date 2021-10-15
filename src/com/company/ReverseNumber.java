1e90e2addd8c6b991dceb9f0ba8b4c6a2aaff0ba
Shivam Singh <shivamfaceuit@gmail.com>
2021-10-15 12:03:18 +0530

    Reverse of a Number using 2nd method

--- src/com/company/ReverseNumber.java
+++ src/com/company/ReverseNumber.java
@@ -5,12 +5,32 @@ public class ReverseNumber {
     public static void main(String[] args) {
         ReverseNo(1824);
         System.out.println(sum);
+        System.out.println( ReverseNo2(1824)) ;
+
+
+    }
+
+   static int ReverseNo2(int n) {
+
+        int digit=(int)(Math.log10(n)+1);
+
+        return Healper(n,digit);
 
 
 
     }
 
-     static int sum=0;
+    static int Healper(int n, int digit) {
+
+        if (n%10==n){
+            return n;
+        }
+        int rem=n%10;
+        return rem* (int) Math.pow(10,digit-1)+Healper(n/10,digit-1);
+    }
+
+
+    static int sum=0;
      static void ReverseNo(int n) {
         if (n==0){
             return;
