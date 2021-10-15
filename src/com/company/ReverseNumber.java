0e1947e28a9afb81219ab3a77ff43d78c143816d
Shivam Singh <shivamfaceuit@gmail.com>
2021-10-15 11:54:59 +0530

    product  of a digit

---
+++ src/com/company/ReverseNumber.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class ReverseNumber {
+
+    public static void main(String[] args) {
+        ReverseNo(1824);
+        System.out.println(sum+" ");
+
+
+
+    }
+
+     static int sum=0;
+     static void ReverseNo(int n) {
+        if (n==0){
+            return;
+        }
+        int rem=n%10;
+        sum=sum*10+rem;
+        ReverseNo(n/10);
+    }
+}
